c3cbafc0dd4bf55f99d5772449f5a03ade1695c4
MarcusGtAccount <pop_marcus_98@yahoo.com>
2016-10-15 14:44:33 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+all: open save
+
+save:
+	@pug index.pug
+	@sass sass-styles/styles.sass > styles.css
+
+open:
+	@google-chrome index.html
---
+++ scripts/functions.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function() {
+	$('#elements-box').autocomplete({
+		source: ["Ana", "are", "mere", "si", "multe", "pere"],
+		minlength: 1
+	})
+});
